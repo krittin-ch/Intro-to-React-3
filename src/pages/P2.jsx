c9b4c5b420c7fe80c27f1d6a35fb54ab691349a0
Krittin Chaowakarn <130139478+krittin-ch@users.noreply.github.com>
2024-03-15 14:56:40 +0700

    Update

--- src/pages/P2.jsx
+++ src/pages/P2.jsx
@@ -2,13 +2,36 @@ import React from "react";
 import P1 from "./P1";
 import logo from "../logo.svg"
 
+// using <ul className=""> becuase this is javascript. It comes from
+// const ul = document.createElement("ul")
+// ul.className = ""
+
 function Header() {
     return (
         <header>
-            <nav>
+            <nav className="nav">
                 <img src={logo} width="100px" alt="logo" />
+                <ul className="nav-items">
+                    <li>Pricing</li>
+                    <li>About</li>
+                    <li>Contact</li>
+                </ul>
             </nav>
         </header>
+    );
+}
+
+function MainContent() {
+    return (
+        <P1 />
+    );
+}
+
+function Footer() {
+    return (
+        <footer>
+            <small>@ 2024 Krittin Development. All rights reserved.</small>
+        </footer>
     );
 }
 
@@ -16,7 +39,8 @@ function P2() {
     return (
         <div>
             <Header />
-            <P1 />
+            <MainContent />
+            <Footer />
         </div>
     );
 }
